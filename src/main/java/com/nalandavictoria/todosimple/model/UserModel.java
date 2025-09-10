681dc4071a7a0671e3d20894b6c6799fa90e06d2
NalandaVictoria <nalanda595@gmail.com>
2025-09-09 22:01:54 -0300

    Initial commit

---
+++ src/main/java/com/nalandavictoria/todosimple/model/UserModel.java
@@ -0,0 +1,34 @@
+package com.nalandavictoria.todosimple.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class UserModel {
+    public interface UserCreate{}
+    public interface UserUpdate{}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    @NotBlank(groups = UserCreate.class)
+    @Length(groups = UserCreate.class, min = 5, max = 150)
+    private String name;
+
+    @Column(name = "password", nullable = false)
+    @NotBlank(groups = {UserCreate.class, UserUpdate.class})
+    @Length(groups = {UserCreate.class, UserUpdate.class}, min = 6, max = 30)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+}
