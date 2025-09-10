7dedf5da0f117479e3e4c377fa3eca94f4a78b26
NalandaVictoria <nalanda595@gmail.com>
2025-09-10 18:34:46 -0300

    Creat UserRepository

---
+++ src/main/java/com/nalandavictoria/todosimple/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.nalandavictoria.todosimple.repository;
+
+import com.nalandavictoria.todosimple.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel,Long> {
+    Optional<UserModel> findByNome (String nome);
+}
